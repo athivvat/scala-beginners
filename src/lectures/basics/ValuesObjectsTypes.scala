d980b5a5b3e64edd9e0d201e573163e9afde1e56
Athiwat Hirunworawongkun <athivvat@gmail.com>
2020-03-09 22:40:44 +0700

    Values, Variables and Types

---
+++ src/lectures/basics/ValuesObjectsTypes.scala
@@ -0,0 +1,30 @@
+package lectures.basics
+
+object ValuesObjectsTypes extends App {
+  val x: Int = 42
+  println(x)
+
+  // VALs are immutable
+  // x = 5
+
+  // COMPILER can infer types
+  // val x = 5
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = true
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 3450
+  val aLong: Long = 2342343320038383L
+  // To create a Long literal you must add L to the end of it.
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // Variables
+  // VARs are mutable
+  var aVariable: int = 4
+
+  aVariable = 5 // side effects
+}
